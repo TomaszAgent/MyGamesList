07831be60bec036929c425e00a7f2b501d377dc3
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2022-12-18 14:19:53 +0100

    Final commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function loginAppear() {
+  document.getElementById("login-window").style.display = "flex";
+}
+function loginDisappear() {
+  document.getElementById("login-window").style.display = "none";
+}
